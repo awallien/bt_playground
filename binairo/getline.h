0864d69890711847b5dbadcfe24ce1c19c37c5d4
awallien <alexswall2016@gmail.com>
2019-05-10 02:35:25 -0400

    updated docstring for getline

--- binairo/getline.h
+++ binairo/getline.h
@@ -3,17 +3,20 @@
 #define __GETLINE_H_
 
 
+///
 /// getline
 ///
-/// acts the same way as getline from POSIX,
-/// for the purpose of compatibility,
+/// for purpose of compatability with Windows mingw,
+/// this getline function has a similar approach to
+/// getline found in the POSIX library
 ///
-/// link: https://riptutorial.com/c/example/8274/get-lines-from-a-file-using-getline--
+/// src: https://riptutorial.com/c/example/8274/get-lines-from-a-file-using-getline--
 ///
 /// @param buf 		buffer to store line
 /// @param size		the initial size pointer to read
 /// @param stream	where to read info from
 /// @return			the number of bytes read
 long int getline( char** buf, size_t* size, FILE* stream ); 
+
 
 #endif //__GETLINE_H_
