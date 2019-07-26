2d380448cb839c81fe0c19d23638ed874ae72d00
awallien <alexswall2016@gmail.com>
2019-07-26 09:22:59 -0700

    implemented __read_config_file, not tested

--- skyscrapers/skyscrapers.py
+++ skyscrapers/skyscrapers.py
@@ -9,49 +9,79 @@ import tkinter
 
 class Skyscrapers:
     def __init__(self, config_file):
-        at_config = 0
-        with open(config_file, "r") as config:
-            for line in config:
-                line = line.split()
-                if not line or "#" in line:
-                    continue
-                if at_config == 0:
-                    self.length, self.width = map(int, line)
-                    self.board = [[0 for _ in range(self.width)] for _ in range(self.length)]
-                elif at_config == 1:
-                    self.north = list(map(int, line))
-                elif at_config == 2:
-                    self.south = list(map(int, line))
-                elif at_config == 3:
-                    self.east = list(map(int, line))
-                elif at_config == 4:
-                    self.west = list(map(int, line))
-                at_config += 1
+        self.dim = None
+        self.board = []
+
+        self.is_marked = []
+
+        self.hints = {}
+
+        self.__read_config_file(config_file)
+
+        assert (all(vars(self).values())), "all values for skyscrapers puzzle are not instantiated"
+
+    def __read_config_file(self, config_file):
+        config = open(config_file)
+        line_no = 0
+        while True:
+            line = config.readline()
+            line_no += 1
+            if not line:
+                break
+            if line == '\n':
+                continue
+            if not self.dim:
+                self.dim = int(config.readline().strip())
+            elif not self.board and not self.is_marked and '.' in line:
+                for i in range(self.dim):
+                    row = line.split()
+                    if len(row) < self.dim:
+                        raise ValueError(f"Configuration file contains invalid width size on line {line_no}")
+                    self.board.append(list(map(lambda x: 0 if x == '.' else int(x), row)))
+                    self.is_marked.append(list(map(lambda x: False if x == '.' else True, row)))
+                    line = config.readline()
+                if sum(self.board) < self.dim or sum(self.board) > self.dim:
+                    raise ValueError("Configuration file contains incorrect number of row markings")
+            elif not self.board and not self.is_marked:
+                self.board = [[0 for _ in range(self.dim)] for _ in range(self.dim)]
+                self.is_marked = [[False for _ in range(self.dim) for _ in range(self.dim)]]
+            elif line.startswith("north: "):
+                self.hints["north"] = list(map(int, line.split()[1:]))
+            elif line.startswith("east: "):
+                self.hints["east"] = list(map(int, line.split()[1:]))
+            elif line.startswith("west: "):
+                self.hints["west"] = list(map(int, line.split()[1:]))
+            elif line.startswith("south: "):
+                self.hints["south"] = list(map(int, line.split()[1:]))
+        config.close()
 
     def mark(self, row, col, number):
         self.board[row][col] = number
 
+    def __str__(self):
+        pass
+
 
 class SkyscrapersSolver:
-    def __init__(self):
-        pass
+    def __init__(self, board):
+        self.board = board
 
     def is_goal(self):
         pass
 
     def is_valid(self):
-        pass
+        return True
 
     def bt(self):
         pass
 
 
 class SkyscrapersUI:
-    def __init__(self, board):
-        self.window = self._mk_window()
-        self.sky_board = board
+    def __init__(self, solver):
+        self.window = self.__mk_window()
+        self.solver = solver
 
-    def _mk_window(self):
+    def __mk_window(self):
         w = tkinter.Tk()
         w.title("Skyscrapers")
         return w
@@ -61,4 +91,4 @@ class SkyscrapersUI:
 
 
 if __name__ == '__main__':
-    pass
+    Skyscrapers("asdf")
